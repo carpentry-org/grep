5f078f034c28f92344155521781212f8f4150384
hellerve <veit@veitheller.de>
2017-12-05 13:01:39 +0100

    grep: dirty but working

--- grep.h
+++ grep.h
@@ -1,11 +1,14 @@
 #include <core.h>
 
 char String_head(string* s) {
+  assert(s != NULL);
   return (*s)[0];
 }
 
 string String_tail(string* s) {
+  assert(s != NULL);
   int len = strlen(*s);
+  assert(len != 0);
   char* news = CARP_MALLOC(len);
   memcpy(news, (*s)+1, len-1);
   news[len-1] = '\0';
